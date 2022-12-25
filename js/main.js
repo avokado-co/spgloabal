30f956844eadfd1ffe5ebf433587f9678962d02e
ravinderatri <ravinderatri86@gmail.com>
2022-12-25 15:19:30 +0530

    final

---
+++ js/main.js
@@ -0,0 +1,33 @@
+function addclass(){  
+   var element = document.getElementById("tabInd");
+   class(element)
+if(element.contains=='current'){
+    element.classList.remove("current");
+}
+else{
+    element.classList.add("current");
+}
+//    .classList.toggle('current');
+    // document.getElementById("tabInd1").classList.toggle('current');
+    // document.getElementById("tabInd2").classList.toggle('current');
+    // document.getElementById("tabInd3").classList.toggle('current');
+}
+// function addclass1(){
+//     document.getElementById("tabInd").classList.toggle('current');
+//     document.getElementById("tabInd1").classList.toggle('current');
+//     document.getElementById("tabInd2").classList.toggle('current');
+//     document.getElementById("tabInd3").classList.toggle('current');
+// }
+// function addclass2(){
+//     document.getElementById("tabInd").classList.toggle('current');
+//     document.getElementById("tabInd1").classList.toggle('current');
+//     document.getElementById("tabInd2").classList.toggle('current');
+//     document.getElementById("tabInd3").classList.toggle('current');
+// }
+// function addclass3(){
+//     document.getElementById("tabInd").classList.toggle('current');
+//     document.getElementById("tabInd1").classList.toggle('current');
+//     document.getElementById("tabInd2").classList.toggle('current');
+//     document.getElementById("tabInd3").classList.toggle('current');
+// }
+ 
